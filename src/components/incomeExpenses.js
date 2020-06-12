093bf628f9c6ef9fb0622e1ff777d7a94df8990f
rabbanirizwan <rabbani_rizwan@yahoo.com>
2020-06-12 17:11:21 +0500

    change color

--- src/components/incomeExpenses.js
+++ src/components/incomeExpenses.js
@@ -19,11 +19,11 @@ export const IncomeExpenses  = ()=>{
     return(
         <div className="inc-exp-container">
         <div>
-          <h4>Income</h4>
+          <h4 style={{color:"black"}}>Income</h4>
           <p id="money-plus" className="money plus">${income}</p>
         </div>
         <div>
-          <h4>Expense</h4>
+          <h4 style={{color:"black"}}>Expense</h4>
           <p id="money-minus" className="money minus">${expense}</p>
         </div>
       </div>
